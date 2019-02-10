9b72995fae9699031cb5cda95154a30c7888dd3e
Erika Secillano <C15339871@mydit.ie>
2019-02-10 01:22:09 +0000

    Database working, log in and sign up working
    hopefully 100%

--- flaskapp/app/website/home.py
+++ flaskapp/app/website/home.py
@@ -5,7 +5,7 @@ from sqlalchemy.orm import scoped_session, sessionmaker
 from passlib.hash import sha256_crypt
 from werkzeug.utils import secure_filename
 
-engine = create_engine("mysql+pymysql://root:12@mbionG@localhost/register")
+engine = create_engine("mysql+pymysql://root:12ambionG@localhost/signup")
 db=scoped_session(sessionmaker(bind=engine))
 
 UPLOAD_FOLDER = 'upload'
@@ -18,13 +18,9 @@ app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 def home():
 	return render_template("main.html")
 
-@app.route('/main', methods=['GET', 'POST'])
+@app.route('/main')
 def main():
-    if request.method == 'POST':
-        return render_template('login.html')
-            
-    elif request.method == 'GET':
-        return render_template('main.html')
+    return render_template("home.html")
 
 @app.route('/signup', methods=['GET', 'POST'])
 def signup():
@@ -32,28 +28,47 @@ def signup():
         email = request.form.get("email")
         username = request.form.get("username")
         password = request.form.get("password")
-        confirm = request.form.get("confirm")
+        #confirm = request.form.get("confirm")
         secure_password = sha256_crypt.encrypt(str(password))
 
-        if password == confirm:
-            db.execute("INSERT INTO users(email,username,password) VALUES (:email, :username, :password)",
-                {"email":email, "username":username,"password":secure_password})
-            db.commit()
-            return redirect(url_for('login'))
-        else:
-            flash("password must match")
-            return render_template("signup.html")
-
+        #if password == confirm:
+        db.execute("INSERT INTO users (email,username,password) VALUES (:email, :username, :password)",
+            {"email":email, "username":username,"password":secure_password})
+        db.commit()
+        return redirect(url_for('login'))
+        #else:
+        #    flash(u'Password must match', 'error')
+        #    return render_template("signup.html")
     return render_template('signup.html')
 
-#@app.route("/logout/")
-#def logout():
-#	session['logged_in'] = False
-#	return login()
+@app.route("/logout")
+def logout():
+    session.clear()
+    flash("You are now logged out")
+    return redirect(url_for('home'))
 
-@app.route('/login')
+@app.route('/login', methods=['GET', 'POST'])
 def login():
-	return render_template('login.html')
+    if request.method == 'POST':
+        username = request.form.get("username")
+        password = request.form.get("password")
+
+        usernamedata = db.execute("SELECT username FROM users WHERE username=:username",{"username":username}).fetchone()
+        passwordata = db.execute("SELECT password FROM users WHERE username=:username",{"username":username}).fetchone()
+
+        if usernamedata is None:
+            flash(u"No username","danger")
+            return render_template("login.html")
+        else:
+            for password_data in passwordata:
+                if sha256_crypt.verify(password,password_data):
+                    session["log"] = True
+                    flash(u"You are logged in","success")
+                    return redirect(url_for("main"))
+                else:
+                    flash(u"incorrect password","danger")
+                    return render_template("login.html")
+    return render_template('login.html')
 
 @app.route('/predict/')
 def predict():
@@ -88,4 +103,5 @@ def upload_file():
     elif request.method == 'GET':
     	return render_template('predict.html')
 if __name__ == '__main__':
+    app.secret_key="this0is1a2pass3word4"
     app.run(debug=True)
